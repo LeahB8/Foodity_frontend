4b43be988641c6329e0c0f3d7cd893a18411fb98
LeahB8 <leahboyd08@gmail.com>
2019-07-15 20:26:29 +0100

    started on homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
+// import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom";
+// import { withRouter } from "react-router-dom";
+import myLogo from './images/myLogo.png'
+import backgroundImage from './images/backgroundImage.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import {
+  validate,
+  fetchUserInfo
+} from "./services/api";
+
+
+class App extends Component {
+  
+  componentDidMount() {
+    if (localStorage.token) {
+      validate().then(data => {
+        if (data.error) {
+          alert(data.error);
+        } else {
+          this.signinAndSetToken(data);
+        }
+      });
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={myLogo} className="App-logo" alt="logo" />
+
+          <button className="signup-in-btn">Sign Up</button>
+          <button className="signup-in-btn">Sign In</button>
+      
+
+         
+        </header>
+        <img src={backgroundImage} className="background-img" alt="background-image" />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,40 @@
+const baseUrl = "http://localhost:3001";
+const signinUrl = baseUrl + "/signin";
+
+export function signin(username, password) {
+  return fetch(signinUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password })
+  }).then(resp => resp.json());
+}
+
+export function validate() {
+  return fetch("http://localhost:3001/validate", {
+    headers: { Authorisation: localStorage.token }
+  }).then(resp => resp.json());
+}
+
+export function createUser(username, password) {
+  return fetch(baseUrl + "/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password
+    })
+  }).then(resp => resp.json());
+}
+
+export function fetchUserInfo(user) {
+  return fetch(baseUrl + `/users/${user.id}`);
+}
+
+export default {
+  signin,
+  validate,
+  createUser,
+  fetchUserInfo,
+};
